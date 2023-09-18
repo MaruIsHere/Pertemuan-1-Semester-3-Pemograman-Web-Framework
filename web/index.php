3c209a9ade447bb7582c9c72778d69fcb9add6c9
Marcell <115938178+MaruIsHere@users.noreply.github.com>
2023-09-18 12:20:39 +0800

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,23 +1,64 @@
 <?php
 
-class Kendaraan {
+class Hewan{
+
    public $nama;
-   public $warna;
-   public $jenisKendaraan;
+   public $spesies;
+   public $habitat;
+   public $berjalan;
+   public $pakan;
+
+
 }
 
-$motor = new Kendaraan();
-$motor->nama = 'Revo';
-$motor->warna = 'Merah';
-$motor->jenisKendaraan = 'Roda Dua';
-echo '<b>Ini adalah : <b>' . $motor->nama . '<br> <b>Berwarna<b>: ' . $motor->warna . '<br><b>Jenis Kendaraan<b> : ' . $motor->jenisKendaraan;
+class Kelelawar extends Hewan {
 
-echo'<br>';
+   public $aktif;
+   public $penglihatan;
 
-$mobil = new Kendaraan();
-$mobil->nama = 'Toyota';
-$mobil->warna = 'Hitam';
-$mobil->jenisKendaraan = 'Roda Empat';
-echo '<b>Ini adalah : <b>' . $mobil->nama . '<br> <b>Berwarna<b>: ' . $mobil->warna . '<br><b>Jenis Kendaraan<b> : ' . $mobil->jenisKendaraan;
+}
+
+class Beruang extends Hewan {
+
+   public $hibernasi;
+   public $bertahanHidup;
+   
+}
+
+$Ayam = new Hewan();
+$Ayam->nama = 'Ayam Boiler';
+$Ayam->spesies = '';
+$Ayam->habitat = '';
+$Ayam->berjalan = '';
+$Ayam->pakan = '';
+
+echo 'Nama Hewan : ' . $Ayam->nama . '<br>Spesies : ' . $Ayam->spesies . '<br>Habitat : ' . $Ayam->habitat . '<br>Berjalan Dengan : ' . $Ayam->berjalan . '<br>Memakan : ' . $Ayam->pakan;
+
+echo 'Nama Hewan : ' . $Ayam->nama . '<br>Spesies : ' . $Ayam->spesies . '<br>Habitat : ' . $Ayam->habitat . '<br>Berjalan Dengan : ' . $Ayam->berjalan . '<br>Memakan : ' . $Ayam->pakan;
+
+$Goa = new Kelelawar();
+$Goa->nama = '';
+$Goa->spesies = '';
+$Goa->habitat = '';
+$Goa->berjalan = '';
+$Goa->pakan = '';
+$Goa->aktif = '';
+$Goa->penglihatan = '';
+
+echo 'Nama Hewan : ' . $Goa->nama . '<br>Spesies : ' . $Goa->spesies . '<br>Habitat : ' . $Goa->habitat . '<br>Berjalan Dengan : ' . $Goa->berjalan . '<br>Memakan : ' . $Goa->pakan . '<br>Aktif Pada Waktu : ' . $Goa->aktif . 'Penglihatan Dengan : ' . $Goa->penglihatan;
+
+
+$Kutub = new Beruang();
+$Kutub->nama = '';
+$Kutub->spesies = '';
+$Kutub->habitat = '';
+$Kutub->berjalan = '';
+$Kutub->pakan = '';
+$Kutub->hibernasi = '';
+$Kutub->$bertahanHidup = '';
+
+echo 'Nama Hewan : ' . $Kutub->nama . '<br>Spesies : ' . $Kutub->spesies . '<br>Habitat : ' . $Kutub->habitat . '<br>Berjalan Dengan : ' . $Kutub->berjalan . '<br>Memakan : ' . $Kutub->pakan . ''
+
+
 
 ?>
